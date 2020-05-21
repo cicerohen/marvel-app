c6c520d82e1cdf83365f381677e1da2468d3b6a4
Cicero Viana <cicerohen@gmail.com>
2020-05-21 16:47:55 -0300

    Fix modal ref

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import ReactModal from "react-modal";
+
 import ReactDOM from "react-dom";
 import AppContainer from "./containers/AppContainer";
 
 import "./scss/main.scss";
+
+ReactModal.setAppElement("#root");
 
 ReactDOM.render(<AppContainer />, document.getElementById("root"));
